a78fc3ec311d109471db2bedf149e259ebc8ad5a
Scott O'Malley <scottyomalley@gmail.com>
2022-12-02 00:21:23 +0000

    Add advent of code day1a

--- day1a/src/main.rs
+++ day1a/src/main.rs
@@ -1,3 +1,16 @@
+
+/* 
+Input is values grouped per elf, blank line = new elf. Find elf carrying most calories (values added together)
+*/
 fn main() {
-    println!("Hello, world!");
+    let file = include_str!("../input.txt");
+    let values: Vec<i32> = file.split("\r\n")
+        .map(|group| 
+            group.lines()
+            .map(|line| line.parse::<i32>().unwrap())
+            .sum::<i32>()
+        ).collect();
+    let max = values.iter().max().unwrap();
+    println!("max: {}", max);
 }
+
